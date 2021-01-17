cf5627684f24fcdaec434ae46a9d5899844acd72
unknown <you@example.com>
2021-01-18 01:54:40 +0600

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(){
+    super()
+    this.state={
+      board : Array(9).fill("X")
+    }
+  }
+  
+  render() {
+
+   const Box= this.state.board.map((box)=>{
+      return(
+        <div className='box'>{box}</div>
+      );
+    })
+
+    return (
+      <div className='container'>
+        <h1>Tic Tac Toe</h1>
+        <div className='board'>
+
+        {Box}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
